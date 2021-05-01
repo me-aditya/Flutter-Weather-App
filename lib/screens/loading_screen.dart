5001958e646e31b8ce64ebba9742bf1720b4d35f
me-aditya <adityarathi1212@gmail.com>
2021-05-01 23:02:35 +0530

    Fetched weather data through openweatherapi added 'dart:convert' to convert string to json data

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/services/location.dart';
 
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Location location = new Location();
+const String apiKey = '565b2883c54ac9bf1d0fb75c6b2b9957';
+
+String url =
+    "https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey";
+
+// String url =
+//     "https://api.openweathermap.org/data/2.5/weather?lat=37.42342342342342&lon=-122.08395287867832&appid=565b2883c54ac9bf1d0fb75c6b2b9957";
+
 void getLocation() async {
-  Location location = new Location();
   await location.getCurrentLocation();
-  print(location.latitude);
-  print(location.longitude);
+}
+
+void getData() async {
+  http.Response response = await http.get(Uri.parse(url));
+  String data = response.body;
+  var longitude = jsonDecode(data)["coord"]["log"];
+  var latitude = jsonDecode(data)["coord"]["lat"];
+
+  print(latitude);
 }
 
 class LoadingScreen extends StatefulWidget {
@@ -26,7 +44,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            getLocation();
+            getData();
           },
           child: Text('Get Location'),
         ),
